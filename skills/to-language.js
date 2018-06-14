1f058c86a980e22d5cf59354558e42709ceb24b3
Vutivi <201141790@student.uj.ac.za>
2018-06-14 14:24:02 +0200

    Updated translation message and added unknown language to fallback

--- skills/to-language.js
+++ skills/to-language.js
@@ -1,19 +1,26 @@
 //
 // Simplest use of Botkit's conversation system
 //
+
 const translate = require('google-translate-api');
 
+//Languages available on Google API
 var languageCodeDict={"afrikaans":"af","albanian":"sq","amharic":"am","arabic":"ar","armenian":"hy","azeerbaijani":"az","basque":"eu","belarusian":"be","bengali":"bn","bosnian":"bs","bulgarian":"bg","catalan":"ca","cebuano":"ceb","chinese(simplified)":"zh-CN","chinese(traditional)":"zh-TW","corsican":"co","croatian":"hr","czech":"cs","danish":"da","dutch":"nl","english":"en","esperanto":"eo","estonian":"et","finnish":"fi","french":"fr","frisian":"fy","galician":"gl","georgian":"ka","german":"de","greek":"el","gujarati":"gu","haitian":"ht","hausa":"ha","hawaiian":"haw","hebrew":"iw","hindi":"hi","hmong":"hmn","hungarian":"hu","icelandic":"is","igbo":"ig","indonesian":"id","irish":"ga","italian":"it","japanese":"ja","javanese":"jw","kannada":"kn","kazakh":"kk","khmer":"km","korean":"ko","kurdish":"ku","kyrgyz":"ky","lao":"lo","latin":"la","latvian":"lv","lithuanian":"lt","luxembourgish":"lb","macedonian":"mk","malagasy":"mg","malay":"ms","malayalam":"ml","maltese":"mt","maori":"mi","marathi":"mr","mongolian":"mn","myanmar":"my","nepali":"ne","norwegian":"no","nyanja":"ny","pashto":"ps","persian":"fa","polish":"pl","portuguese":"pt","punjabi":"pa","romanian":"ro","russian":"ru","samoan":"sm","scots-gaelic":"gd","serbian":"sr","sesotho":"st","shona":"sn","sindhi":"sd","sinhala":"si","slovak":"sk","slovenian":"sl","somali":"so","spanish":"es","sundanese":"su","swahili":"sw","swedish":"sv","tagalog":"tl","tajik":"tg","tamil":"ta","telugu":"te","thai":"th","turkish":"tr","ukrainian":"uk","urdu":"ur","uzbek":"uz","vietnamese":"vi","welsh":"cy","xhosa":"xh","yiddish":"yi","yoruba":"yo","zulu":"zu"};
 
 module.exports = function (controller) {
 	for(var key in languageCodeDict){
 		controller.hears(["to-"+key], 'direct_message,direct_mention', function (bot, message) {
 			
+			//get target language only from message
 			var target=message.text.match("(to-)(\\w+)")[2];
+			
 			bot.startConversation(message, function (err, convo) {
+				
 				//call the google translate API to translate the message only
 				translate(message.text.split(target)[1], {to: languageCodeDict[target]}).then(res => {
-					convo.say("This message is for you:"+res.text);
+					
+					convo.say("Message translated to "+target+":\n\n"+res.text);
+					
 				}).catch(err => {
 				console.error(err);
 				});
--- skills/z-fallback.js
+++ skills/z-fallback.js
@@ -4,9 +4,15 @@
 module.exports = function (controller) {
 
     controller.hears([".*"], 'direct_message,direct_mention', function (bot, message) {
-        var mardown = "Sorry, I did not understand your request.<br/>Try "
+		if(message.text.indexOf("to-") >=0){
+			markdown="Sorry, I did not understand your language. I only understand the languages within the Google Translate API which can be viewed from <a href='https://cloud.google.com/translate/docs/languages'>here</a>.";
+			bot.reply(message, markdown);
+		}
+		else{
+			var markdown = "Sorry, I did not understand your request.<br/>Try "
             + bot.appendMention(message, "help");
             
-        bot.reply(message, mardown);
+			bot.reply(message, markdown);
+		}
     });
 }
