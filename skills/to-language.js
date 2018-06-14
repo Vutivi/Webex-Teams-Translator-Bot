f533a6f6c7c8e394ab705a9372735a2bb6d3f777
Vutivi <201141790@student.uj.ac.za>
2018-06-14 13:37:32 +0200

    updated command to-language

--- skills/to-language.js
+++ skills/to-language.js
@@ -5,22 +5,14 @@ const translate = require('google-translate-api');
 
 var languageCodeDict={"afrikaans":"af","albanian":"sq","amharic":"am","arabic":"ar","armenian":"hy","azeerbaijani":"az","basque":"eu","belarusian":"be","bengali":"bn","bosnian":"bs","bulgarian":"bg","catalan":"ca","cebuano":"ceb","chinese(simplified)":"zh-CN","chinese(traditional)":"zh-TW","corsican":"co","croatian":"hr","czech":"cs","danish":"da","dutch":"nl","english":"en","esperanto":"eo","estonian":"et","finnish":"fi","french":"fr","frisian":"fy","galician":"gl","georgian":"ka","german":"de","greek":"el","gujarati":"gu","haitian":"ht","hausa":"ha","hawaiian":"haw","hebrew":"iw","hindi":"hi","hmong":"hmn","hungarian":"hu","icelandic":"is","igbo":"ig","indonesian":"id","irish":"ga","italian":"it","japanese":"ja","javanese":"jw","kannada":"kn","kazakh":"kk","khmer":"km","korean":"ko","kurdish":"ku","kyrgyz":"ky","lao":"lo","latin":"la","latvian":"lv","lithuanian":"lt","luxembourgish":"lb","macedonian":"mk","malagasy":"mg","malay":"ms","malayalam":"ml","maltese":"mt","maori":"mi","marathi":"mr","mongolian":"mn","myanmar":"my","nepali":"ne","norwegian":"no","nyanja":"ny","pashto":"ps","persian":"fa","polish":"pl","portuguese":"pt","punjabi":"pa","romanian":"ro","russian":"ru","samoan":"sm","scots-gaelic":"gd","serbian":"sr","sesotho":"st","shona":"sn","sindhi":"sd","sinhala":"si","slovak":"sk","slovenian":"sl","somali":"so","spanish":"es","sundanese":"su","swahili":"sw","swedish":"sv","tagalog":"tl","tajik":"tg","tamil":"ta","telugu":"te","thai":"th","turkish":"tr","ukrainian":"uk","urdu":"ur","uzbek":"uz","vietnamese":"vi","welsh":"cy","xhosa":"xh","yiddish":"yi","yoruba":"yo","zulu":"zu"};
 
-
 module.exports = function (controller) {
-for(var key in languageCodeDict){
-    controller.hears("to-"+key, 'direct_message,direct_mention', function (bot, message) {
-		//call the google translate API to translate the message only
-		translate(message, {to: languageCodeDict[key]}).then(res => {
-			bot.startConversation(message, function (err, convo) {
-				convo.say("Message translated to "+key+" is: "+ res.text);
-                convo.next(); 
+
+    controller.hears([/^to-/w+/], 'direct_message,direct_mention', function (bot, message) {
+		
+        bot.startConversation(message, function (err, convo) {
+            convo.say(message);
+	
         });
-			
-		}).catch(err => {
-			console.error(err);
-		});
 
     });
-}
 };
-
